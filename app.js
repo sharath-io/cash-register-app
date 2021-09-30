25061f7baa8d2f30585e8213dd3554ce401d7c7b
Sharath <sharath.esr@gmail.com>
2021-09-30 16:02:02 +0530

    style: added styling and code beautify done

--- app.js
+++ app.js
@@ -7,28 +7,22 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-function validateInputs() 
-{
-    if (billAmount.value > 0) 
-    {
+function validateInputs() {
+    if (billAmount.value > 0) {
         if (Number(cashAmount.value) >= Number(billAmount.value)) {
             const changeAmount = cashAmount.value - billAmount.value;
             calculateChange(changeAmount);
-        } 
-        else
+        } else
             message.innerText = "Cash amount should be equal or greater than bill amount ";
-    } 
-    else {
+    } else {
         message.innerText = "Bill Amount should be greater than 0";
     }
 }
 
-function calculateChange(changeAmount)
-{
+function calculateChange(changeAmount) {
     //by dividing we get number of notes required of that particular denomination
     //by modulous we get the left out changeAmount that to be passed to next denomination
-    for(let i=0;i<availableNotes.length; i++)
-    {
+    for (let i = 0; i < availableNotes.length; i++) {
         const numberOfNotes = Math.trunc(changeAmount / availableNotes[i]);
         changeAmount = changeAmount % availableNotes[i];
         noOfNotes[i].innerText = numberOfNotes;
